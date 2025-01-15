f6bf80995034ed8a0d6365a81aefffb503360827
deparra <deparra@wisc.edu>
2025-01-14 18:03:32 -0600

    learned illumination

--- save_learned_code.py
+++ save_learned_code.py
@@ -1,10 +1,20 @@
 import numpy as np
-from models.CM1DLayers import CorrelationMatrixLayer
+from models.CM1DLayers import IlluminationLayer
+import os
 
-init = 'checkpoints/good_checkpoints/coded_model-tmp.ckpt'
+init = 'checkpoints/coded_model-v5.ckpt'
+filename = init.split('/')[-1].split('.')[0]
+cmat_folder = 'C:\\Users\\Patron\\PycharmProjects\\Indirect-Direct-ToF\\learned_codes\\coding_matrices'
+illum_folder = 'C:\\Users\\Patron\\PycharmProjects\\Indirect-Direct-ToF\\learned_codes\\illumination'
 
-coding_mat = CorrelationMatrixLayer(init=init, get_from_model=True)
+
+coding_mat = IlluminationLayer(init=init, get_from_model=True)
 
 cmat = np.transpose(np.squeeze(coding_mat.cmat1D.weight.data.detach().clone().numpy()))
+illum = np.squeeze(coding_mat.illumination.data.detach().numpy())
 
-np.save(f'C:\\Users\\Patron\\PycharmProjects\\Indirect-Direct-ToF\\learned_codes\\{init.split('/')[-1]}', cmat)
+print(cmat.shape)
+print(illum.shape)
+np.save(os.path.join(cmat_folder, filename), cmat)
+np.save(os.path.join(illum_folder, filename), illum)
+
